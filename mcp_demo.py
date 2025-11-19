9aef0dc74382c71409840178d566b2457476a3aa
swapnildagade1213 <111967597+swapnildagade1213@users.noreply.github.com>
2025-11-19 15:20:51 +0530

    Delete unused company and MCP code fetching functions
    Removed get_companycode and get_MCPcode functions.

--- mcp_demo.py
+++ mcp_demo.py
@@ -84,30 +84,6 @@ def fetch_graphToken(saltdata : str, keydata : str)-> str:
         return f"Token not reterived"
 
 @mcp.tool()
-def get_companycode()-> str:
-    """
-    Fetch company code
-    
-    Args: None
-        
-    Returns:
-        str: company code
-    """
-    return "#123456"
-
-@mcp.tool()
-def get_MCPcode()-> str:
-    """
-    Fetch MCP code
-    
-    Args: None
-        
-    Returns:
-        str: MCP code
-    """
-    return "MCP#123@456"
-
-@mcp.tool()
 async def get_userUpcomingMeetings(email: str, saltdata : str, keydata : str ) -> str:
     """Get upcoming meetings of a user for the next specified days.
 
